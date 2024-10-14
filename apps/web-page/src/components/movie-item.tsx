08110553fc1feb377f76dc1fc3c5b25df3ba750b
heston-pablo <heston.pablo@gmail.com>
2024-10-14 06:44:46 +0000

    mission-05

---
+++ apps/web-page/src/components/movie-item.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import styles from "./movie-item.module.css";
+
+import type { Movie } from "@/types";
+
+type MovieItemProps = { movie: Movie };
+
+export default function MovieItem({ movie }: MovieItemProps) {
+  return (
+    <Link href={`/movie/${movie.id}`}>
+      <img src={movie.posterImgUrl} className={styles.poster_img} />
+    </Link>
+  );
+}
